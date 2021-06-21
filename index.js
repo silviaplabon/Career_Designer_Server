e076826740cde5f5b019fd1203f4bc554c008f6d
silviaplabon <silviaplabon@gmail.com>
2021-06-21 15:16:40 +0600

    filter job done

--- index.js
+++ index.js
@@ -12,71 +12,114 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcsxh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const EmployerCollection = client.db('Career_Designer').collection("Employer_Collection");
-  const AllJobsCollection = client.db('Career_Designer').collection("AllJobCollection");
-  const AdminsCollection = client.db('Career_Designer').collection("AdminCollection");
-
-//   app.post('/adddata/PopularDrink', (req, res) => {
-//     const newProduct = req.body;
-//     console.log(newProduct);
-//     console.log(req.body, "come from client site")
-//     AllJobsCollection.insertOne(newJob)
-//       .then(result => {
-//         console.log('inserted count', result.insertedCount);
-//         res.send(result.insertedCount > 0)
-//       })
-//   })
-  app.post('/adding_a_employer', (req, res) => {
-    const newemployer = req.body;
-    console.log(newemployer);
-    EmployerCollection.insertOne(newemployer)
-      .then(result => {
-        console.log('inserted count', result.insertedCount);
-        console.log(result);
-        res.send(result.insertedCount > 0)
-      })
-  })
+    const EmployerCollection = client.db('Career_Designer').collection("Employer_Collection");
+    const AllJobsCollection = client.db('Career_Designer').collection("AllJobCollection");
+    const AdminsCollection = client.db('Career_Designer').collection("AdminCollection");
+    const JobSeekersCollection=client.db('Career_Designer').collection("JobSeekerCollection")
 
 
-  app.get('/userIsEmployer', (req, res) => {
-    EmployerCollection.find({ email: req.query.email })
-      .toArray((err, documents) => {
-          console.log(err)
-          console.log(documents)
-        res.send(documents);
-      })
-  });
+    app.post('/adding_a_employer', (req, res) => {
+        const newemployer = req.body;
+        console.log(newemployer);
+        EmployerCollection.insertOne(newemployer)
+            .then(result => {
+                console.log('inserted count', result.insertedCount);
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
 
 
-  app.post('/employer_adding_a_job', (req, res) => {
-    const newjob = req.body;
-    console.log(newjob);
-    AllJobsCollection.insertOne(newjob)
-      .then(result => {
-        console.log('inserted count', result.insertedCount);
-        console.log(result);
-        res.send(result.insertedCount > 0)
-      })
-  })
-  app.get('/userIsAdmin', (req, res) => {
-    AdminsCollection.find({ email: req.query.email })
-      .toArray((err, documents) => {
-        res.send(documents);
-      })
-  });
-  app.post('/newAdminMaker', (req, res) => {
-    const newAdmin = req.body;
-    console.log(req.body, "come from client site")
-    AdminsCollection.insertOne(newAdmin)
-      .then(result => {
-        console.log('inserted count', result.insertedCount);
-        res.send(result.insertedCount > 0)
-      })
-  })
+    app.get('/userIsEmployer', (req, res) => {
+        EmployerCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                console.log(err)
+                console.log(documents)
+                res.send(documents);
+            })
+    });
 
 
-  
+    app.post('/employer_adding_a_job', (req, res) => {
+        const newjob = req.body;
+        console.log(newjob);
+        AllJobsCollection.insertOne(newjob)
+            .then(result => {
+                console.log('inserted count', result.insertedCount);
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+    app.get('/userIsAdmin', (req, res) => {
+        AdminsCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
+    app.post('/newAdminMaker', (req, res) => {
+        const newAdmin = req.body;
+        console.log(req.body, "come from client site")
+        AdminsCollection.insertOne(newAdmin)
+            .then(result => {
+                console.log('inserted count', result.insertedCount);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/allJobList', (req, res) => {
+        AllJobsCollection.find({}).toArray((err, documents) => {
+            console.log(err)
+            // console.log(documents)
+            res.send(documents);
+        })
+    });
+
+    app.get('/searchJobData/:category/:title/:address', (req, res) => {
+        const name = req.params.title;
+        const address = req.params.address;
+        const area = req.params.category;
+        console.log(name, address, area);
+        
+        AllJobsCollection.find({
+            $and: [
+                { $or: [ {"category":area } ] },
+                { $or: [ {"location":address },{ "title":name } ] }
+            ]
+        })
+            .toArray((err, products) => {
+                // console.log(products, "silvia")
+                res.send(products)
+            })
+    })
+    app.get('/filterJobData/:category/:title/:address', (req, res) => {
+        const name = req.params.title;
+        const address = req.params.address;
+        const area = req.params.category;
+        console.log(name, address, area);
+        AllJobsCollection.find({ category: { $ne: area } })
+            .toArray((err, filterjob) => {
+                console.log(filterjob?.length, "silvia")
+                res.send(filterjob)
+            })
+    })
+    app.post('/adding_a_job_seeker_profile', (req, res) => {
+        const newProfile = req.body;
+        console.log(newProfile);
+        JobSeekersCollection.insertOne(newProfile)
+            .then(result => {
+                console.log('inserted count', result.insertedCount);
+                console.log(result);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+
+
+
+
+
+
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
